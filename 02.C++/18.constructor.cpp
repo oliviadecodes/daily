3e2c357656150ba0739a8987b7099d48f404a46c
Olivia <1024827983@qq.com>
2020-11-14 22:19:57 +0800

    构造与析构函数

---
+++ 02.C++/18.constructor.cpp
@@ -0,0 +1,76 @@
+/*************************************************************************
+	> File Name: 18.constructor.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: 日 10/ 4 10:58:04 2020
+ ************************************************************************/
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <cmath>
+#include <queue>
+using namespace std;
+
+struct A {
+    A() : next(nullptr) {
+        cout << this << " default constructor" << endl;
+    }
+    A(A *obj) {
+        this->next = obj;
+        this->next->arr = new int[1000];
+    }
+    A(int n) : next(nullptr) {
+        cout << this << " an int constructor" << endl;
+        x = n, y = n;
+    }
+    A(const A &obj) : next(nullptr) {
+        cout << this << " copy constructor" << endl;
+        this->x = obj.x, this->y = obj.y;
+    }
+    void output() {
+        cout << this->x << " " << this->y << endl;
+    }
+    int x, y;
+    int *arr;
+    A *next;
+    ~A() {
+        if (this->next) {
+            delete this->next->arr;
+        }
+        cout << this << " destructor" << endl;
+    }
+};
+
+void func(int &x) {
+    x += 5;
+}
+
+A aa;
+A bb(&aa);
+A cc(&bb);
+
+int main() {
+    int n = 7;
+    func(n);
+    cout << n << endl;
+    cout << "pre position" << endl;
+    A a;
+    A b = 8;
+    A c(a);
+    cout << "a = " << &a << endl;
+    cout << "b = " << &b << endl;
+    cout << "c = " << &c << endl;
+    cout << "aa = " << &aa << endl;
+    cout << "bb = " << &bb << endl;
+    cout << "cc = " << &cc << endl;
+    b.output();
+    b = 6;
+    b.output();
+    cout << "next position" << endl;
+    return 0;
+}
